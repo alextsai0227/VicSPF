f72cd7c478b02dd139a4b241b0b01b903cb179e3
Alex Tsai <alextsai0227@gmail.com>
2019-08-03 15:35:42 +1000

    try to connect on mongodb cluster

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(
+    "mongodb+srv://alextsai:ji394xk4xk4@cluster0-vypaz.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+var db = mongoose.connection;
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("were connected");
+
+});
 
 const hostname = '127.0.0.1';
 const port = 3005;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
